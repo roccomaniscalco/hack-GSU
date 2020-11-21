8b71d6e0bf146f731200ccc1ea33ccc2d9be87f3
Rocco <roccomaniscalco2001@gmail.com>
2020-11-21 16:55:21 -0500

    implemented feed navigation

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
 const API_KEY = "1746e141194bb3c5a7187530792f8912";
-let state = "GA";
-let relevantBills;
-let recentBills;
+const state = "GA";
+const bills = [];
 
 $(window).on("load", () => {
-  const loadRecentBills = () => {
-    $.ajax({
-      url: `https://api.legiscan.com/?key=${API_KEY}&op=getMasterList&state=${state}`,
-      method: "GET",
-    }).then((response) => {
-      recentBills = sortByDate(response.masterlist);
-      displayFeed(recentBills);
-    });
+  // Get Relevant Searches
+  $.ajax({
+    url: `https://api.legiscan.com/?key=${API_KEY}&op=search&state=${state}`,
+    method: "GET",
+  }).then((response) => {
+    getBills(Object.values(response.searchresult));
+  });
 
-    const sortByDate = (obj) => {
-      return Object.values(obj)
-        .sort((a, b) => new Date(a.last_action_date) - new Date(b.last_action_date))
-        .reverse();
-    };
-  };
-
-  const loadRelevantBills = () => {
-    $.ajax({
-        url: `https://api.legiscan.com/?key=${API_KEY}&op=search&state=${state}`,
+  // Get Specific Bill Info
+  const getBills = (searches) => {
+    console.log(searches);
+    for (let i = 0; i < searches.length - 1; i++) {
+      $.ajax({
+        url: `https://api.legiscan.com/?key=${API_KEY}&op=getBill&id=${searches[i].bill_id}`,
         method: "GET",
       }).then((response) => {
-        relevantBills = Object.values(response.searchresult);
-        displayFeed(relevantBills);
+        bills.push(response.bill);
+        appendToFeed(response.bill, i);
       });
+    }
   };
 
-  const getBill = (billID) => {
-    $.ajax({
-      url: `https://api.legiscan.com/?key=${API_KEY}&op=getgetBill&id=${billID}`,
-      method: "GET",
-    }).then(function (response) {});
+  // Display Bills In Feed
+  const appendToFeed = (bill, i) => {
+    const status = decodeBillStatus(bill.status)
+    $("nav").append(
+      `<div class="card" data-bill-index="${i}">
+        <div>
+          <h1>${bill.bill_number}</h1>
+          <span class="${status}">${status}</span>
+        </div>
+        <p>${bill.title}</p>
+      </div>`
+    );
   };
 
-  const displayFeed = (bills) => {
-    console.log(bills);
+  // Convert Status Number To Feed
+  const decodeBillStatus = (statusNum) => {
+    switch (statusNum) {
+      case 1:
+        return "Introduced";
+      case 2:
+        return "Engrossed";
+      case 3:
+        return "Enrolled";
+      case 4:
+        return "Passed";
+      case 5:
+        return "Vetoed";
+      case 6:
+        return "failed";
+    }
   };
 
-  $("#recent").on("click", () => {
-    if (recentBills == undefined) loadRecentBills();
-    else displayFeed(recentBills);
-  });
-  $("#relevant").on("click", () => {
-    if (relevantBills == undefined) loadRelevantBills();
-    else displayFeed(relevantBills);
+  // Register User Interaction
+  $("nav").on("click", "div", function () {
+    $("nav").children().removeClass("selected");
+    $(this).addClass("selected");
   });
 });
